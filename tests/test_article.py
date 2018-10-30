1928a1879413b0edf12b1deb0e68d228625cf7ee
Vivien Ngo <v.ngo@columbia.edu>
2018-10-30 19:46:08 -0400

    Fix test names

--- tests/test_article.py
+++ tests/test_article.py
@@ -1,13 +1,13 @@
 """
 This test checks parsing functionality of the Article class
 """
-import requests
-
-import os,sys
+import os
+import sys
 sys.path.insert(1, os.path.join(sys.path[0], '..'))
+import requests
 import Article
 
-def init_test():
+def test_init():
     """
     This test ensures that an Article object is initialized
     and the url is parsed correctly
@@ -15,18 +15,21 @@ def init_test():
     url = 'www.cnn.com/2018/09/25/health/iyw-girl-named-florence-collects-donations-trnd/index.html'
     article = Article.Article(url)
     assert article
-    assert article.source_url == 'www.cnn.com'
+    assert article.source_url == 'http://www.cnn.com'
     assert article.url == '/2018/09/25/health/iyw-girl-named-florence-collects-donations-trnd/index.html'
 
-def time_test():
+def test_time():
     """ This test ensures that the time estimate is reasonable """
     url = 'www.cnn.com/2018/09/25/health/iyw-girl-named-florence-collects-donations-trnd/index.html'
     article = Article.Article(url)
+    article.build()
+    assert article.time
     assert article.time > 0 and article.time <= 5
 
-def top_img_test():
+def test_top_img():
     """ This ensures that the top image url exists """
     url = 'www.cnn.com/2018/09/25/health/iyw-girl-named-florence-collects-donations-trnd/index.html'
     article = Article.Article(url)
+    article.build()
     request = requests.get(article.source_url + article.url)
     assert request.status_code == 200
