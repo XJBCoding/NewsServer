9e1466479256a122be350aa80deec222b5d9b64c
Vivien Ngo <v.ngo@columbia.edu>
2018-10-30 19:33:48 -0400

    Fix test

--- tests/test_article.py
+++ tests/test_article.py
@@ -13,7 +13,7 @@ def init_test():
     and the url is parsed correctly
     """
     url = 'www.cnn.com/2018/09/25/health/iyw-girl-named-florence-collects-donations-trnd/index.html'
-    article = Article(url)
+    article = Article.Article(url)
     assert article
     assert article.source_url == 'www.cnn.com'
     assert article.url == '/2018/09/25/health/iyw-girl-named-florence-collects-donations-trnd/index.html'
@@ -21,12 +21,12 @@ def init_test():
 def time_test():
     """ This test ensures that the time estimate is reasonable """
     url = 'www.cnn.com/2018/09/25/health/iyw-girl-named-florence-collects-donations-trnd/index.html'
-    article = Article(url)
+    article = Article.Article(url)
     assert article.time > 0 and article.time <= 5
 
 def top_img_test():
     """ This ensures that the top image url exists """
     url = 'www.cnn.com/2018/09/25/health/iyw-girl-named-florence-collects-donations-trnd/index.html'
-    article = Article(url)
+    article = Article.Article(url)
     request = requests.get(article.source_url + article.url)
     assert request.status_code == 200
