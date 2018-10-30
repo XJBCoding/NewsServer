7aa5413b586f23b5848fd78da4be8eca4c14dc85
Vivien Ngo <v.ngo@columbia.edu>
2018-10-30 15:32:15 -0400

    Add Article test

--- README.md
+++ README.md
@@ -8,4 +8,6 @@ pip install -r requirements/dev.txt
 ```
 
 ## Testing
-For pre-commit testing, we use [git-pylint-commit-hook](https://git-pylint-commit-hook.readthedocs.io/en/latest/usage.html)
+For pre-commit testing, we use [git-pylint-commit-hook](https://git-pylint-commit-hook.readthedocs.io/en/latest/usage.html).
+
+For post-commit CI, we use [Travis](https://travis-ci.com/XJBCoding/NewsServer).
---
+++ tests/test_article.py
@@ -0,0 +1,29 @@
+"""
+This test checks parsing functionality of the Article class
+"""
+import requests
+import Article
+
+def init_test():
+    """
+    This test ensures that an Article object is initialized
+    and the url is parsed correctly
+    """
+    url = 'www.cnn.com/2018/09/25/health/iyw-girl-named-florence-collects-donations-trnd/index.html'
+    article = Article(url)
+    assert article
+    assert article.source_url == 'www.cnn.com'
+    assert article.url == '/2018/09/25/health/iyw-girl-named-florence-collects-donations-trnd/index.html'
+
+def time_test():
+    """ This test ensures that the time estimate is reasonable """
+    url = 'www.cnn.com/2018/09/25/health/iyw-girl-named-florence-collects-donations-trnd/index.html'
+    article = Article(url)
+    assert article.time > 0 and article.time <= 5
+
+def top_img_test():
+    """ This ensures that the top image url exists """
+    url = 'www.cnn.com/2018/09/25/health/iyw-girl-named-florence-collects-donations-trnd/index.html'
+    article = Article(url)
+    request = requests.get(article.source_url + article.url)
+    assert request.status_code == 200
