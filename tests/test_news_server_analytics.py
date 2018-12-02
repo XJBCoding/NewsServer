2b77b3bfc79496b7b71bc0390a6adcb4ad18fd51
Vivien Ngo <v.ngo@columbia.edu>
2018-12-02 00:34:19 -0500

    Test analytics

--- tests/test_news_server_analytics.py
+++ tests/test_news_server_analytics.py
@@ -10,6 +10,7 @@ from news_server import create_app
 import pytest
 import unittest
 from datetime import date,timedelta
+import pymongo
 
 
 class FlaskClientTest(unittest.TestCase):
@@ -22,23 +23,18 @@ class FlaskClientTest(unittest.TestCase):
         self.app_context.push()
         self.client = self.app.test_client()
 
+        mongo_client = pymongo.MongoClient("mongodb+srv://news:123@cluster0-avowj.mongodb.net/test?retryWrites=true")
+        self.db = mongo_client["newsapp"]
+
         self.user_with_history = 'testing_username_with_history'
         self.user_no_history = 'testing_username_no_history'
 
     def tearDown(self):
         self.app_context.pop()
 
-    '''
-    This is a test to make sure that the user cannot view analytics without logging in
-    '''
-    def test_add_history_login(self):
-        resp = self.client.post('/add-history')
-        assert b'You are not logged in' in resp.data
-
     def test_add_history(self):
         username = self.user_with_history
-
-        user_history = self.app.db["user_history"]
+        user_history = self.db["user_history"]
         user_history.remove({'username':username})  # clear history from previous tests
 
         with self.client.session_transaction() as sess:
@@ -48,9 +44,34 @@ class FlaskClientTest(unittest.TestCase):
         # need to assert that the data is in MongoDB
         assert b'Added article to DB' in resp.data
 
-    # def test_analytics_time(self):
-    #     # TODO: after setting up MongoDB code, need to pass dummy data in database
-    #     with self.client.session_transaction() as sess:
-    #         sess['username'] = 'testing'
-    #     resp = self.client.get('/analytics')
-    #     assert b'mins' or b'minutes' in resp.data
+    def test_analytics_time_with_history(self):
+        username = self.user_with_history
+        user_history = self.db["user_history"]
+
+        with self.client.session_transaction() as sess:
+            sess['username'] = username
+
+        resp = self.client.get('/analytics')
+        assert b'10 min' in resp.data
+
+    def test_analytics_topic_with_history(self):
+        username = self.user_with_history
+        user_history = self.db["user_history"]
+
+        with self.client.session_transaction() as sess:
+            sess['username'] = username
+
+        resp = self.client.get('/analytics')
+        assert b'health' in resp.data
+
+    def test_analytics_no_history(self):
+        username = self.user_no_history
+        user_history = self.db["user_history"]
+
+        with self.client.session_transaction() as sess:
+            sess['username'] = username
+
+        resp = self.client.get('/analytics')
+        assert not b'mins' in resp.data
+
+    
